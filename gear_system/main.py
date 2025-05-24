19ec13c33fb74203c02e628335861eeb2dacc478
Detryxx <kelebeni2010@gmail.com>
2025-05-24 23:12:28 +0200

    Solved gear_system

--- gear_system/main.py
+++ gear_system/main.py
@@ -1,44 +1,5 @@
 import random as rnd
 import ast
-#
-# def left_lever(current_state: list) -> list:
-#   current_state[0] += 1
-#   current_state[1] += 1
-#   return current_state
-#
-# def right_lever(current_state: list) -> list:
-#   current_state[1] += 1
-#   current_state[2] += 1
-#   return current_state
-#
-# def trial(current_state: list) -> list:
-#   lor = rnd.randint(1, 2)
-#   if lor == 1:
-#     current_state = left_lever(current_state)
-#   elif lor == 2:
-#     current_state= right_lever(current_state)
-#   print(current_state, "asdasd")
-#   return current_state
-#
-# def gear_system(end_result: list, max_moves: int = 8,):
-#   first = 3
-#   second = 3
-#   third = 3
-#   left = first + 1, second + 1
-#   right = second + 1, third + 1
-#   counter = 0
-#   states = [first, second, third]
-#   required_moves = []
-#   while end_result != states:
-#     for x in range(0, 3):
-#       print(x, "ketiksz")
-#       states[x] = (states[x] - 1) % 4
-#       print(x, "iksz")
-#     states = trial(states)
-#   print(states)
-#
-#
-# gear_system([2, 1, 2])
 
 def left(first, second, third, x, moves):
   first += 1
