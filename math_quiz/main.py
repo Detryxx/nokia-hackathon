17de7b7771c9b1896b601dee3cd8f1e4878b0ff7
Detryxx <kelebeni2010@gmail.com>
2025-05-26 17:23:01 +0200

    Made math_quiz's main.py file adhere to PEP8 and added comments

--- math_quiz/main.py
+++ math_quiz/main.py
@@ -1,19 +1,32 @@
+# pylint: disable=C0103
+"""
+    random - needed for the randomization of die rolls in exercise 7
+    scipy - needed for the root_scalar for the second exercise
+    sympy - needed for solving linear equations (exercise 5, 6)
+
+    comments - or rather my thought process - can be found in the thinking.txt file as described per
+    the exercise
+"""
+import random
 from scipy.optimize import root_scalar
 import sympy
-import random
+
 
 #first
 R = sympy.symbols('R')
 radius = sympy.Eq((((R - 2) / R) ** 2) + (((R - 1) / R) ** 2), 1)
 solution_1 = sympy.solve(radius)
 for i in solution_1:
-    if i == 1:                      #this is because cos != 0 and if R = 1 then (R - 1) / R = 1 - 1 / 1 = 0 / 1 = 0
+    if i == 1:  #this is because cos != 0 and if R = 1 then (R - 1) / R = 1 - 1 / 1 = 0 / 1 = 0
         solution_1.remove(i)
 print(f"1.: {solution_1[0]}")
 
 
 #second
 def f(x):
+    """
+        Defines the function f(x) for the second exercise
+    """
     return 4**x + 6**x - 9**x
 
 solution = root_scalar(f, bracket=[1, 2], method="brentq")
@@ -21,17 +34,20 @@ print(f"2.: {round(solution.root, 6)}")
 
 
 #third
-x = 1
+y = 1
 solution_3 = 0
-while x <= 99:
-    solution_3 += (1 / ((x ** 0.5) +((x + 1) ** 0.5)))
-    x = x + 1
+while y <= 99:
+    solution_3 += (1 / ((y ** 0.5) +((y + 1) ** 0.5)))
+    y = y + 1
 print(f"3.: {round(solution_3)}")
 
 
 #fourth
-def g(x):
-    return (x**x - 1)**2
+def g(z):
+    """
+        Defines the function g(z) for the fourth exercise
+    """
+    return (z**z - 1)**2
 
 solution = root_scalar(g, bracket=[0.5, 1.5], method="newton", x0 = 1)
 print(f"4.: {solution.root}")
@@ -63,7 +79,7 @@ print(f"6.: {solution_6[t]}")
 Anna = []
 Balazs = []
 winners = []
-for x in range(1250000):
+for i in range(1250000):
     while True:
         dice_roll = random.randint(1, 6)
         Anna.append(dice_roll)
